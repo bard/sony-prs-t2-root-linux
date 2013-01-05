6cd923760b0fc3bcc2b4e69f24c678f9874373c3
Massimiliano Mirra <bard@hyperstruct.net>
2013-01-05 21:38:02 +0000

    Refactor. Use rupor-* variants. Add enable-db and minimal-root steps. Refine output. Temporarily disable backup step.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-sony-prs-t2-root-linux
-======================
+*Warning: work in progress*. Published for feedback and review. Do NOT
+ use yet.
 
-Scripts to simplify rooting of Sony PRS-T2 E-reader from Linux
+These scripts aim to automate as much as possible the rooting process
+of the Sony PRS-T2 e-reader from Linux, and possibly provide a
+foundation for similar efforts on other devices.
+
+Significant limitations:
+
+- assumes firmware 1.0.04.11081 and does not check it
+- assumes European model
+
+If you know how to check firmware and model of a non-rooted device
+from Linux, let me know.
+
+
--- common.sh
+++ common.sh
@@ -1,17 +1,33 @@
-abort() {
-    MESSAGE=$1
+TEXT_CYAN='\e[0;36m'
+TEXT_RED='\e[0;31m'
+TEXT_GREEN='\e[0;32m'
+TEXT_PLAIN='\e[0m'
+TEXT_BOLD='\e[1m'
+
+
+explain() {
     echo
-    echo "############################################################"
-    echo "## ERROR: $MESSAGE. Exiting."
+    echo -e "${TEXT_CYAN}trace${TEXT_PLAIN}: $1"
+}
+
+inform() {
+    echo
+    echo -e "${TEXT_GREEN}info${TEXT_PLAIN}: ${TEXT_BOLD}$1${TEXT_PLAIN}"
+}
+
+error() {
+    echo
+    echo -e "${TEXT_RED}error${TEXT_PLAIN}: $1"
+}
+
+abort() {
+    error "$1"
     echo
     exit 1
 }
 
 finish() {
-    MESSAGE=$1
-    echo
-    echo "############################################################"
-    echo "## SUCCESS: $MESSAGE."
+    inform "$1"
     echo
     exit 0
 }
@@ -26,12 +42,64 @@ announce_step() {
 
 check_command() {
     COMMAND=$1
-
-    echo -n "Checking for '$COMMAND'... "
+    
+    echo -n "Looking for '$COMMAND'... "
     if which $COMMAND >/dev/null; then
         echo "found."
     else
         echo
-        abort "Command '$COMMAND' not found or not in search path. Please install before proceeding"
+        abort "Command '$COMMAND' not found or not in search path. Please install before proceeding."
     fi
+}
+
+detect_disk_reader() {
+    for DEV in /dev/sd?; do
+        if udisks --show-info $DEV | grep PRS-XXX >/dev/null; then
+            abort "Reader found but in recovery mode. Reboot in normal mode and retry."
+        fi
+        
+        if udisks --show-info $DEV | egrep 'model:\s+PRS-T2$' >/dev/null; then
+            if udisks --show-info $DEV | egrep 'label:\s+READER$' >/dev/null; then
+                echo $DEV
+                return 0
+            fi
+        fi
+    done
+}
+
+detect_disk_sd() {
+    for DEV in /dev/sd??; do
+        if udisks --show-info $DEV | grep 'by-id/usb-Sony_PRS-T2_SD' >/dev/null; then
+            if udisks --show-info $DEV | grep 'partition:$' >/dev/null; then
+                echo $DEV
+                return 0
+            fi
+        fi
+    done
+    return 1
+}
+
+prs_mount_reader() {
+    DEV=$(detect_disk_reader)
+    udisks --mount $DEV >/dev/null
+    udisks --show-info $DEV | grep 'mount paths:' | tr -d ' ' | cut -d: -f2
+}
+
+prs_unmount_reader() {
+    sync
+    DEV=$(detect_disk_reader)
+    udisks --unmount $DEV
+}
+
+prs_mount_sd() {
+    DEV=$(detect_disk_sd) || return 1
+
+    udisks --mount $DEV >/dev/null || return 1
+    udisks --show-info $DEV | grep 'mount paths:' | tr -d ' ' | cut -d: -f2
+}
+
+prs_unmount_sd() {
+    sync
+    DEV=$(detect_disk_sd) || return 1
+    udisks --unmount $DEV
 }
